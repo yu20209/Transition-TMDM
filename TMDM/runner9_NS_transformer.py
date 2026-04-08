144b95157896314faef01b615d9ea7be2a7d307d
Yuxuan <2329952783@qq.com>
2026-04-08 14:59:22 +0800

    Update runner9_NS_transformer.py

--- TMDM/runner9_NS_transformer.py
+++ TMDM/runner9_NS_transformer.py
@@ -8,23 +8,23 @@ import setproctitle
 if __name__ == '__main__':
     setproctitle.setproctitle('LYX_thread')
 
-    parser = argparse.ArgumentParser(description='Non-stationary Transformers for Time Series Forecasting')
+    parser = argparse.ArgumentParser(description='Transition TMDM: trend decomposition + residual TMDM')
 
     # basic config
     parser.add_argument('--is_training', type=bool, default=True, help='status')
-    parser.add_argument('--model_id', type=str, default='ETTh2_96_192', help='model id')
-    parser.add_argument('--model', type=str, default='ETTh2_96_192',
-                        help='model name, options: [ns_Transformer, Transformer]')
+    parser.add_argument('--model_id', type=str, default='ETTh2_96_192_transition_tmdm', help='model id')
+    parser.add_argument('--model', type=str, default='Transition_TMDM',
+                        help='model name')
 
     # data loader
     parser.add_argument('--data', type=str, default='custom', help='dataset type')
     parser.add_argument('--root_path', type=str, default='./dataset/ETT-small/', help='root path of the data file')
     parser.add_argument('--data_path', type=str, default='ETTh2.csv', help='data file')
     parser.add_argument('--features', type=str, default='M',
-                        help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
+                        help='forecasting task, options:[M, S, MS]')
     parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
     parser.add_argument('--freq', type=str, default='h',
-                        help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+                        help='freq for time features encoding')
     parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
 
     # forecasting task
@@ -44,29 +44,33 @@ if __name__ == '__main__':
     parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
     parser.add_argument('--factor', type=int, default=3, help='attn factor')
     parser.add_argument('--distil', action='store_false',
-                        help='whether to use distilling in encoder, using this argument means not using distilling',
+                        help='whether to use distilling in encoder',
                         default=True)
     parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
     parser.add_argument('--embed', type=str, default='timeF',
-                        help='time features encoding, options:[timeF, fixed, learned]')
+                        help='time features encoding')
     parser.add_argument('--activation', type=str, default='gelu', help='activation')
     parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
     parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
 
-    parser.add_argument('--k_z', type=float, default=1e-2, help='KL weight 1e-9')
-    parser.add_argument('--k_cond', type=float, default=1, help='Condition weight')
-    parser.add_argument('--d_z', type=int, default=8, help='KL weight')
+    parser.add_argument('--k_z', type=float, default=1e-2, help='KL weight')
+    parser.add_argument('--k_cond', type=float, default=1.0, help='Residual condition weight')
+    parser.add_argument('--k_trend', type=float, default=1.0, help='Trend supervision weight')
+    parser.add_argument('--d_z', type=int, default=8, help='latent dim placeholder')
 
+    # === 新增：趋势分解 / 趋势预测相关参数 ===
+    parser.add_argument('--trend_kernel', type=int, default=25, help='moving average kernel for trend decomposition')
+    parser.add_argument('--trend_individual', type=bool, default=True, help='channel-wise trend linear head')
 
     # optimization
     parser.add_argument('--num_workers', type=int, default=4, help='data loader num workers')
     parser.add_argument('--itr', type=int, default=1, help='experiments times')
     parser.add_argument('--train_epochs', type=int, default=200, help='train epochs')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')  # 32
-    parser.add_argument('--test_batch_size', type=int, default=8, help='batch size of train input data')  # 32
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--test_batch_size', type=int, default=8, help='batch size of test input data')
     parser.add_argument('--patience', type=int, default=15, help='early stopping patience')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate for diffusion model')
-    parser.add_argument('--learning_rate_Cond', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--learning_rate_Cond', type=float, default=0.0001, help='not used in transition version')
     parser.add_argument('--des', type=str, default='Exp', help='exp description')
     parser.add_argument('--loss', type=str, default='mse', help='loss function')
     parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
@@ -84,24 +88,16 @@ if __name__ == '__main__':
                         help='hidden layer dimensions of projector (List)')
     parser.add_argument('--p_hidden_layers', type=int, default=2, help='number of hidden layers in projector')
 
-    # CART related args
+    # diffusion related args
     parser.add_argument('--diffusion_config_dir', type=str, default='./model9_NS_transformer/configs/toy_8gauss.yml',
                         help='')
-
-    # parser.add_argument('--cond_pred_model_dir', type=str,
-    #                     default='./checkpoints/cond_pred_model_pertrain_NS_Transformer/checkpoint.pth', help='')
     parser.add_argument('--cond_pred_model_pertrain_dir', type=str,
                         default='./checkpoints/cond_pred_model_pertrain_NS_Transformer/checkpoint.pth', help='')
 
     parser.add_argument('--CART_input_x_embed_dim', type=int, default=32, help='feature dim for x in diffusion model')
     parser.add_argument('--mse_timestep', type=int, default=0, help='')
-
     parser.add_argument('--MLP_diffusion_net', type=bool, default=False, help='use MLP or Unet')
-
-    # Some args for Ax (all about diffusion part)
-    parser.add_argument('--timesteps', type=int, default=1000, help='')
-
-
+    parser.add_argument('--timesteps', type=int, default=1000, help='diffusion timesteps')
 
     args = parser.parse_args()
 
@@ -134,7 +130,6 @@ if __name__ == '__main__':
 
     if args.is_training:
         for ii in range(args.itr):
-            # setting record of experiments
             setting = '{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
                 args.model_id,
                 args.model,
@@ -153,12 +148,11 @@ if __name__ == '__main__':
                 args.distil,
                 args.des, ii)
 
-            exp = Exp(args)  # set experiments
+            exp = Exp(args)
             print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
             exp.train(setting)
 
             print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-            # exp.test_cond(setting)
             exp.test(setting)
 
             if args.do_predict:
@@ -168,25 +162,25 @@ if __name__ == '__main__':
             torch.cuda.empty_cache()
     else:
         ii = 0
-        setting = '{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(args.model_id,
-                                                                                                      args.model,
-                                                                                                      args.data,
-                                                                                                      args.features,
-                                                                                                      args.seq_len,
-                                                                                                      args.label_len,
-                                                                                                      args.pred_len,
-                                                                                                      args.d_model,
-                                                                                                      args.n_heads,
-                                                                                                      args.e_layers,
-                                                                                                      args.d_layers,
-                                                                                                      args.d_ff,
-                                                                                                      args.factor,
-                                                                                                      args.embed,
-                                                                                                      args.distil,
-                                                                                                      args.des, ii)
+        setting = '{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
+            args.model_id,
+            args.model,
+            args.data,
+            args.features,
+            args.seq_len,
+            args.label_len,
+            args.pred_len,
+            args.d_model,
+            args.n_heads,
+            args.e_layers,
+            args.d_layers,
+            args.d_ff,
+            args.factor,
+            args.embed,
+            args.distil,
+            args.des, ii)
 
-        exp = Exp(args)  # set experiments
+        exp = Exp(args)
         print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-        exp.test_cond(setting, test=1)
         exp.test(setting, test=1)
         torch.cuda.empty_cache()
